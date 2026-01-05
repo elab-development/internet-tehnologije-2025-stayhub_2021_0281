63752ed69de245a9efa0b8c92a7e210a12fb46e3
Aleksa Obradović <mitaraselara@gmail.com>
2026-01-05 12:00:00 +0000

    Konfiguracija okruzenja podesena

--- stayhub/next.config.ts
+++ stayhub/next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "loremflickr.com" },
+      { protocol: "https", hostname: "picsum.photos" },
+    ],
+  },
+   async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
